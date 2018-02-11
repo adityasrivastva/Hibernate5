9e4a96c3d6a47fd494e528a151ffb14b1a7e0853
adityasrivastva <aditya5srivastva@gmail.com>
2018-02-11 14:33:12 +0530

    HibernateDAOImlementation using DAO design pattern

---
+++ HibernateDAOImplementation/src/main/java/com/aditya/hibernate/client/ClientTest.java
@@ -0,0 +1,45 @@
+package com.aditya.hibernate.client;
+
+import java.util.Date;
+
+import com.aditya.hibernate.model.Employee;
+import com.aditya.hibernate.service.EmployeeService;
+import com.aditya.hibernate.service.impl.EmployeeServiceImpl;
+public class ClientTest {
+	public static void main(String[] args) {
+			EmployeeService employeeService= new EmployeeServiceImpl();
+			
+			//createEmployee(employeeService);
+			//getEmployeeById(employeeService);
+			//updateEmployee(employeeService);
+			deleteEmployeeById(employeeService);
+			
+	}
+
+	private static void deleteEmployeeById(EmployeeService employeeService) {
+		employeeService.deleteEmloyeeById(2);
+		
+	}
+
+	private static void updateEmployee(EmployeeService employeeService) {
+		employeeService.updateEmployeeById(1, 56000.00);
+	}
+
+	private static void getEmployeeById(EmployeeService employeeService) {
+		Employee employee= employeeService.getEmployeeById(3);
+		System.out.println(employee);
+	}
+
+	private static void createEmployee(EmployeeService employeeService) {
+		employeeService.addEmployee(getEmployee());
+	}
+
+	private static Employee getEmployee() {
+		Employee employee = new Employee();
+		employee.setEmployeeName("Priya Srivastva");
+		employee.setEmail("priya@gmail.com");
+		employee.setSalary(50000.00);
+		employee.setDoj(new Date());
+		return employee;
+	}
+}
